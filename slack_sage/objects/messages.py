4ccb0812061cd1dda0d885249e7fb68cee77251e
Adam <adam@northisup.com>
2017-04-11 14:25:36 -0700

    Add additional attributes to the message object

--- slack_sage/objects/messages.py
+++ slack_sage/objects/messages.py
@@ -7,11 +7,17 @@ class Message(api.APIContext):
     user = None
     channel = None
     text = None
+    ts = None
+    source_team = None
+    team = None
 
     @staticmethod
     def parse(obj):
         return Message(
             channel=channels.Channel(channel_id=obj.get('channel')),
             user=user.SlackUser(user_id=obj.get('user')),
-            text=obj.get('text')
+            text=obj.get('text'),
+            ts=obj.get('ts'),
+            team=obj.get('team'),
+            source_team=obj.get('source_team'),
         )
